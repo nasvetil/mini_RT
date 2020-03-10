dd29a3c779de96f24113b51b876891db55bb0b09
Drahff Orange dartwing <dorange-@oa-i5.21-school.ru>
2020-03-10 15:54:22 +0300

    Add parse child tags

--- includes/parser.h
+++ includes/parser.h
@@ -17,9 +17,12 @@ typedef struct	s_xml_attr
 
 typedef struct	s_xml_elem
 {
-	char*		name;
-	char*		ptr_begin;
-	char*		ptr_end;
+	char		*name;
+	char		*ptr_begin;
+	char		*ptr_end;
+	char		*ptr_begin_content;
+	char		*ptr_end_content;
+	char		*content;
 	t_list		*attr;
 	t_list		*child;
 }				t_xml_elem;
--- src/parser/parser_general.c
+++ src/parser/parser_general.c
@@ -1,66 +1,93 @@
 #include "doom.h"
 
-void		parser_xml_get_open_tag(char *file, t_xml_elem *elem)
+int			parser_xml_get_open_tag(char *file_start, char *file_end, t_xml_elem *elem)
 {
 	char		*start_tag;
+	char		*start_close_tag;
 	char		*end_tag;
 	size_t		len;
 
-	start_tag = ft_strstr(file, "<");
+	start_tag = ft_strstr(file_start, "<");
+	start_close_tag = ft_strstr(file_start, "</");
+	if (start_tag == NULL || \
+		start_tag > file_end || \
+		start_close_tag == NULL || \
+		start_tag == start_close_tag)
+		return (0);
 	end_tag = ft_strstr(start_tag, ">");
-	if (start_tag == NULL || end_tag == NULL)
-		ft_error("[XML Parser]: Syntax error");
+	if (start_tag == NULL || end_tag == NULL || end_tag > file_end)
+		ft_error("[XML Parser]: Syntax error: open tag");
 	elem->ptr_begin = start_tag;
 	start_tag++;
 	len = end_tag - start_tag;
 	elem->name = ft_strncpy(ft_strnew(len), start_tag, len);
+	end_tag++;
+	elem->ptr_begin_content = end_tag;
+	return (1);
 }
 
-void		parser_xml_get_close_tag(t_xml_elem *elem)
+void		parser_xml_get_close_tag(char *file_start, char *file_end, t_xml_elem *elem)
 {
 	char	*file;
 	char	*close_tag_str;
 	char	*ptr_tag_str;
 	char	*close_tag;
 
-	file = elem->ptr_begin;
+	file = file_start;
 	close_tag_str = ft_strjoin("</", elem->name);
 	ptr_tag_str = close_tag_str;
 	close_tag_str = ft_strjoin(close_tag_str, ">");
 	free(ptr_tag_str);
 	close_tag = ft_strstr(file, close_tag_str);
-	if (close_tag == NULL)
-		ft_error("[XML Parser]: Syntax error");
+	if (close_tag == NULL || close_tag > file_end)
+		ft_error("[XML Parser]: Syntax error: close tag");
 	elem->ptr_end = ft_strstr(close_tag, ">");
 	elem->ptr_end++;
 	free(close_tag_str);
+	elem->ptr_end_content = close_tag;
 }
 
-t_xml_elem*	parser_xml_get_elem(char *file)
+t_xml_elem*	parser_xml_get_elem(char *file_start, char *file_end)
 {
 	t_xml_elem	*elem;
+	size_t		len;
 
 	elem = ft_memalloc(sizeof(t_xml_elem));
-	parser_xml_get_open_tag(file, elem);
-	parser_xml_get_close_tag(elem);
+	if (parser_xml_get_open_tag(file_start, file_end, elem))
+	{
+		parser_xml_get_close_tag(file_start, file_end, elem);
+		elem->content = NULL;
+	}
+	else
+	{
+		elem->name = NULL;
+		elem->ptr_begin = file_start;
+		elem->ptr_end = file_end;
+		elem->ptr_begin_content = file_start;
+		elem->ptr_end_content = file_end;
+		len = elem->ptr_begin - elem->ptr_end;
+	}
 	return (elem);
 }
 
-int			parser_xml_file_struct(char *file, t_list *s)
+int			parser_xml_file_struct(char *file_start, char *file_end, t_list *s)
 {
 	t_xml_elem	*elem;
 	t_list		*list;
 
-	elem = parser_xml_get_elem(file);
+	elem = parser_xml_get_elem(file_start, file_end);
 	list = ft_lstnew(elem, sizeof(t_xml_elem));
 	if (s == NULL)
 		s = list;
 	else
 		ft_lstadd(&s, list);
-	if (*elem->ptr_end == '\0')
+	printf("@ %s: %s\n", elem->name, elem->content);
+	if (elem->ptr_end_content - elem->ptr_begin_content && elem->name != NULL)
+		parser_xml_file_struct(elem->ptr_begin_content, elem->ptr_end_content, NULL);
+	if (*elem->ptr_end == '\0' || elem->name == NULL)
 		return (0);
 	else
-		return (parser_xml_file_struct(elem->ptr_end, s));
+		return (parser_xml_file_struct(elem->ptr_end, file_end, s));
 }
 
 char*		parser_xml_get_single_line_file(char *filename)
@@ -99,14 +126,15 @@ char*		parser_xml_get_single_line_file(char *filename)
 void*		parser_xml_file(char *filename)
 {
 	char	*file;
+	char	*file_end;
 	t_list	*s;
 
 	file = parser_xml_get_single_line_file(filename);
+	file_end = ft_strchr(file, '\0');
 	if ((void*)file == NULL)
 		ft_error("error!");
-	printf("%s\n", file);
 	s = NULL;
-	parser_xml_file_struct(file, s);
+	parser_xml_file_struct(file, file_end, s);
 	free(file);
 	return (NULL);
 }
